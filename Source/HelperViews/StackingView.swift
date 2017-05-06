a03d66fed82d5655dbded86d5d09427501d853d3
Eli Bishop <this@errorbar.net>
2017-05-05 23:10:31 -0700

    add support for margins between views

--- Source/HelperViews/StackingView.swift
+++ Source/HelperViews/StackingView.swift
@@ -24,6 +24,20 @@ import Foundation
 
 @objc(StackingView)
 public class StackingView: NSView {
+  @IBInspectable public var margin: CGFloat {
+    get {
+      return _margin
+    }
+    set(m) {
+      if m != _margin {
+        _margin = m
+        if inited {
+          updateLayout()
+        }
+      }
+    }
+  }
+  private var _margin: CGFloat = 0
   private var inited: Bool = false
   private var updating: Bool = false
   private var dirty: Bool = false
@@ -40,7 +54,7 @@ public class StackingView: NSView {
                                              name: NSNotification.Name.NSViewFrameDidChange, object: view)
     }
     super.addSubview(view)
-    if (inited) {
+    if inited {
       updateLayout()
     }
   }
@@ -67,7 +81,7 @@ public class StackingView: NSView {
   }
   
   func subviewFrameChanged(_ sender: Any) {
-    if (updating) {
+    if updating {
       dirty = true
     }
     else {
@@ -76,7 +90,7 @@ public class StackingView: NSView {
   }
 
   func updateLayout() {
-    if (updating) {
+    if updating {
       return
     }
     updating = true
@@ -93,11 +107,14 @@ public class StackingView: NSView {
         if !v.isHidden {
           let min = minimumHeight(v)
           let max = maximumHeight(v)
-          minSpaceUsed += min
+          minSpaceUsed += min + _margin
           if (max > min) {
             varHeightCount += 1
           }
         }
+      }
+      if minSpaceUsed > 0 {
+        minSpaceUsed -= _margin
       }
       let availableHeight: CGFloat = frame.size.height - minSpaceUsed
       var pos: CGFloat = frame.size.height
@@ -107,12 +124,12 @@ public class StackingView: NSView {
           let min = minimumHeight(v)
           let max = maximumHeight(v)
           if (min == max) {
-            newHeight = min;
+            newHeight = min
           }
           else {
             newHeight = min + (availableHeight / CGFloat(varHeightCount))
             if (newHeight > max) {
-              newHeight = max;
+              newHeight = max
             }
           }
           let newFrame = NSMakeRect(0, pos - newHeight, fixedWidth, newHeight)
@@ -121,7 +138,7 @@ public class StackingView: NSView {
             v.frame = newFrame
             changed = true
           }
-          pos -= newHeight;
+          pos -= newHeight + _margin
         }
       }
       
